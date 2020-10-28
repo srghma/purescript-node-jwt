023c252622c071161500abaf2738268c49e7a349
Serhii Khoma <srghma@gmail.com>
2020-10-28 20:53:23 +0200

    feat: add verify

--- src/Node/Jwt.purs
+++ src/Node/Jwt.purs
@@ -3,39 +3,10 @@ module Node.Jwt
   , module Exports
   ) where
 
-import Control.Monad.Except
-import Data.Bifunctor
-import Data.Either
-import Data.Function.Uncurried
-import Data.List.NonEmpty
-import Data.Maybe
-import Data.Traversable
-import Effect.Aff
-import Effect.Uncurried
-import Foreign
-import Node.Jwt.Types
-import Prelude
-
-import Data.Argonaut (Json)
-import Data.Array.NonEmpty (NonEmptyArray)
-import Data.Foldable as Foldable
-import Data.JSDate (JSDate)
-import Data.JSDate as JSDate
-import Data.NonEmpty (NonEmpty(..), (:|))
-import Data.NonEmpty as NonEmpty
-import Data.Nullable (Nullable)
-import Data.Nullable as Nullable
-import Effect.Aff.Compat (EffectFnAff(..), fromEffectFnAff)
-import Node.Jwt.JsonWebTokenError (JsonWebTokenError)
-import Node.Jwt.JsonWebTokenError as JsonWebTokenError
-import Node.Jwt.NotBeforeError (NotBeforeError)
-import Node.Jwt.NotBeforeError as NotBeforeError
-import Node.Jwt.NumericDate (NumericDate)
+import Node.Jwt.JsonWebTokenError (JsonWebTokenError) as Exports
+import Node.Jwt.NotBeforeError (NotBeforeError) as Exports
+import Node.Jwt.TokenExpiredError (TokenExpiredError) as Exports
 import Node.Jwt.NumericDate as Exports
-import Node.Jwt.NumericDate as NumericDate
-import Node.Jwt.TokenExpiredError (TokenExpiredError)
-import Node.Jwt.TokenExpiredError as TokenExpiredError
 import Node.Jwt.Types as Exports
-import Unsafe.Coerce (unsafeCoerce)
-import Node.Jwt.JwtErrors
+import Node.Jwt.JwtErrors as Exports
 import Node.Jwt.Verify as Exports
